d59c2a21da1d113bea95eb1132ac22268894c6f8
Bryan Novak <>
2018-06-27 23:38:46 -0500

    adding stop/start

--- src/App.js
+++ src/App.js
@@ -4,15 +4,39 @@ import axios from 'axios';
 
 axios({
   method:'get',
-  url:'/test'
-}).then(function(response) {
+  url:'http://localhost:3777/test'
+}).then(function (response) {
   console.log(response);
 });
 
 class App extends Component {
+
+  start = () => {
+    console.log('start');
+    axios({
+      method:'post',
+      url:'http://localhost:3777/start'
+    }).then(function (response) {
+      console.log(response);
+    });    
+  };
+
+  stop = () => {
+    console.log('stop');
+    axios({
+      method:'post',
+      url:'http://localhost:3777/stop'
+    }).then(function (response) {
+      console.log(response);
+    });    
+  };
+
   render() {
     return (
       <div className="">
+        <h1>TradeBot</h1>
+        <button onClick={this.start}>start</button>
+        <button onClick={this.stop}>stop</button>
       </div>
     );
   }
